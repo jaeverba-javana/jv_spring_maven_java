9475a7e9d86b7c8b14b27439f528085649b92e62
jaeverba-javana <jaeverba@gmail.com>
2022-11-03 03:55:43 -0500

    Avance en el desarrollo, sección habilidades y acerca de mí mejoradas

--- src/main/java/com/jaeverba/jv/controller/SolicitudController.java
+++ src/main/java/com/jaeverba/jv/controller/SolicitudController.java
@@ -21,20 +21,19 @@ public class SolicitudController {
 
         int resultado = solicitudService.createSolicitud(solicitud);
 
-        boolean bul = resultado == 1? Email.emailContactMeConfirm(solicitud).send() : false;
+        boolean enviado = resultado == 1? Email.emailContactMeConfirm(solicitud).send() : false;
 
-        boolean borrado = resultado == 1 && !bul? solicitudService.delete(solicitud.getEmail()) : false;
+        //boolean borrado = resultado == 1 && !bul? solicitudService.delete(solicitud.getEmail()) : false;
+        //boolean borrado = resultado == 1 && !bul && solicitudService.delete(solicitud.getEmail());
+        if (resultado == 1 && !enviado) solicitudService.delete(solicitud.getEmail());
 
         //return String.valueOf(bul);
-        return (resultado == 1) && bul? "1" : "0";
+        return (resultado == 1) && enviado? "1" : "0";
     }
 
     @RequestMapping(value = "/form/solicitud", method = RequestMethod.DELETE, params = "email")
     public String delete(@RequestParam String email) {
-
-        boolean borrado = solicitudService.delete(email);
-
-        return borrado? "1" : "0";
+        return solicitudService.delete(email)? "1" : "0";
     }
 
     @RequestMapping(value = "/form/solicitudes", method = RequestMethod.GET)
